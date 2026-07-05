25a0761c1ad6304e087d73e8f338974744a6d05a
Meyggy <mmeyggy@gmail.com>
2026-07-05 21:22:32 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise'); 
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'mysql.railway.internal',
@@ -7,4 +7,5 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+
 module.exports = connection;
