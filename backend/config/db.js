d5170cddd887f725ae15713752451a392daf230e
Meyggy <mmeyggy@gmail.com>
2026-07-05 20:36:26 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,9 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-const db = mysql.createPool({
- host: process.env.MYSQLHOST,
- user: process.env.MYSQLUSER,
- password: process.env.MYSQLPASSWORD,
- database: process.env.MYSQL_DATABASE,
- port: process.env.MYSQLPORT,
- waitForConnections: true,
- connectionLimit: 10,
- queueLimit: 0,
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'mysql.railway.internal',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-module.exports = db;
