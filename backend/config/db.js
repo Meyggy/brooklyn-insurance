36ac85df0091cb41a5b13c33beebd3f5b05c4528
Meyggy <mmeyggy@gmail.com>
2026-07-07 22:01:11 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise'); 
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'mysql.railway.internal',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+async function createConnection() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  return connection;
+}
 
-module.exports = connection;
+module.exports = createConnection();
